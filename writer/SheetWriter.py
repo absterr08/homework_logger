468668cfed6b641f5dcb4aedaec7dc8a3e62f3a9
BCrawfordScott <bscott@appacademy.io>
2018-02-09 14:37:27 -0500

    Fix merge conflict

--- writer/SheetWriter.py
+++ writer/SheetWriter.py
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-
-from __future__ import print_function
-import httplib2
-import os
-=======
 from __future__ import print_function
 import httplib2
 import os
 import pdb
 import re
->>>>>>> 2eff14d36d8d8e4442717470e01a3a9754b62cf7
 
 from apiclient import discovery
 from oauth2client import client
@@ -22,74 +15,6 @@ try:
 except ImportError:
     flags = None
 
-<<<<<<< HEAD
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Sheets API Python Quickstart'
-
-
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'sheets.googleapis.com-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
-
-def main():
-    """Shows basic usage of the Sheets API.
-
-    Creates a Sheets API service object and prints the names and majors of
-    students in a sample spreadsheet:
-        https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                    'version=v4')
-    service = discovery.build('sheets', 'v4', http=http,
-                              discoveryServiceUrl=discoveryUrl)
-
-    API_KEY = 'AIzaSyDRhoY4b__NXarftJhj0urTkgVq53ssvVM'
-    # spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-    spreadsheetId = '1-b_qTGM-QpBv7AyXF4PHLDEMFNI3g8TL72ZsRFeBj2Q'
-    # rangeName = 'Class Data!A2:B'
-    rangeName = 'A2:B'
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheetId, range=rangeName, key=API_KEY).execute()
-    values = result.get('values', [])
-
-    if not values:
-        print('No data found.')
-    else:
-        print('Name,        Email')
-        # print('Name, Email, W1D1, W2D2')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s,  %s' % (row[0], row[1]))
-=======
 
 class SheetWriter:
 # If modifying these scopes, delete your previously saved credentials
@@ -157,7 +82,6 @@ def main():
     cells = ['X1', 'X3', 'X5', 'X7', 'X9']
     writer.markCells(cells)
 
->>>>>>> 2eff14d36d8d8e4442717470e01a3a9754b62cf7
 
 
 if __name__ == '__main__':
